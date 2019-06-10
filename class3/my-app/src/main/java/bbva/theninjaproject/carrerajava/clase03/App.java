d890273ddbb789ca43c459f7c52559d6cf6caf1d
Pabloin <pablo.inchausti.bf@gmail.com>
2019-06-09 22:43:24 -0300

    #3 Add Step 2 con Junit

--- class3/my-app/src/main/java/bbva/theninjaproject/carrerajava/clase03/App.java
+++ class3/my-app/src/main/java/bbva/theninjaproject/carrerajava/clase03/App.java
@@ -1,6 +1,7 @@
 package bbva.theninjaproject.carrerajava.clase03;
 
 import bbva.theninjaproject.carrerajava.clase03.step1.PersonaStep1;
+import bbva.theninjaproject.carrerajava.clase03.step2.PersonaStep2;
 
 /**
  * Hello world!
@@ -13,6 +14,7 @@ public class App
         System.out.println( "Hello World!" );
         
         ejemplo_step1();
+        ejemplo_step2();
     }
     
     
@@ -21,13 +23,27 @@ public class App
      */
     public static void ejemplo_step1()
     {
-    	System.out.println( "App.ejemplo_step1()" );
+    	System.out.println( "App.ejemplo_step1()");
     	
         PersonaStep1 p1 = new PersonaStep1();
         p1.nombre = "Mariano";
         
         PersonaStep1 p2 = new PersonaStep1();
         p2.nombre = "Jorge";
+        		
+        p1.saludar();
+        p2.saludar();
+    }
+    
+    /**
+     * STEP 2: · Ejemplo de constructor básico.
+     */
+    public static void ejemplo_step2()
+    {
+    	System.out.println( "App.ejemplo_step2()");
+    	
+    	PersonaStep2 p1 = new PersonaStep2("Mariano");
+        PersonaStep2 p2 = new PersonaStep2("Jorge");
         		
         p1.saludar();
         p2.saludar();
