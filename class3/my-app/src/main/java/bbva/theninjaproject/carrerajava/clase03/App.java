46b789baa149e254b1304461d1c5c0adb746baa3
Pabloin <pablo.inchausti.bf@gmail.com>
2019-06-09 22:11:17 -0300

    #3 Add Saludar

--- class3/my-app/src/main/java/bbva/theninjaproject/carrerajava/clase03/App.java
+++ class3/my-app/src/main/java/bbva/theninjaproject/carrerajava/clase03/App.java
@@ -1,5 +1,6 @@
 package bbva.theninjaproject.carrerajava.clase03;
 
+
 /**
  * Hello world!
  *
@@ -9,5 +10,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        
+        Persona p1 = new Persona();
+        p1.nombre = "Mariano";
+        
+        Persona p2 = new Persona();
+        p2.nombre = "Jorge";
+        		
+        p1.saludar();
+        p2.saludar();
     }
 }
---
+++ class3/my-app/src/main/java/bbva/theninjaproject/carrerajava/clase03/Persona.java
@@ -0,0 +1,11 @@
+package bbva.theninjaproject.carrerajava.clase03;
+
+public class Persona {
+	
+	String nombre;
+	
+	public void saludar() {
+		System.out.println("Hola " + this.nombre);
+	}
+
+}
