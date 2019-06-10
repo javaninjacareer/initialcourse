05f7a5cb4fcbc4e4036a7a648ad9ecc1ce7bb429
Pabloin <pablo.inchausti.bf@gmail.com>
2019-06-09 22:21:25 -0300

    #3 Add ejemplo Step 1

--- class3/my-app/src/main/java/bbva/theninjaproject/carrerajava/clase03/App.java
+++ class3/my-app/src/main/java/bbva/theninjaproject/carrerajava/clase03/App.java
@@ -1,5 +1,6 @@
 package bbva.theninjaproject.carrerajava.clase03;
 
+import bbva.theninjaproject.carrerajava.clase03.step1.PersonaStep1;
 
 /**
  * Hello world!
@@ -11,11 +12,21 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        
-        Persona p1 = new Persona();
+        ejemplo_step1();
+    }
+    
+    
+    /**
+     * STEP 1: Ejemplo de varias instancias de una misma clase.
+     */
+    public static void ejemplo_step1()
+    {
+    	System.out.println( "App.ejemplo_step1()" );
+    	
+        PersonaStep1 p1 = new PersonaStep1();
         p1.nombre = "Mariano";
         
-        Persona p2 = new Persona();
+        PersonaStep1 p2 = new PersonaStep1();
         p2.nombre = "Jorge";
         		
         p1.saludar();
