48e5d4dc58f6d85bb2bf26891b662dcf35f628f6
javacha <javacha.ux@gmail.com>
2019-07-12 01:49:43 -0300

    carga inicial clase 5

---
+++ class5/my-app/src/test/java/bbva/theninjaproject/carrerajava/clase05/Ejercicio1Test.java
@@ -0,0 +1,35 @@
+package bbva.theninjaproject.carrerajava.clase05;
+
+import bbva.theninjaproject.carrerajava.clase05.Ejercicio1;
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class Ejercicio1Test extends TestCase
+{
+
+	
+    
+    public void testEjercicio2AlRevesadorDeFrasesInvierteBienUnaFrase()
+    {
+    	String frase = "Hola que tal!";
+        //assertEquals("!lat euq aloH", Ejercicio2.alRevesadorDeFrases(frase) );
+    }    
+    
+
+    
+    
+    /**
+     * Probando Ejercicio2 SolucionUno
+     */
+    public void testEjercicio2SolucionUnoInvierteBienUnaFrase()
+    {
+    	String frase = "Hola que tal!";
+        //assertEquals("!lat euq aloH", Ejercicio2.solucionUno(frase) );
+    }
+    
+
+    
+    
+}
