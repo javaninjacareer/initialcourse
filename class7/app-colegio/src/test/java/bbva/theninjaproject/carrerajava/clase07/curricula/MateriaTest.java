8685d652edbb5d1a4c7693c8fc3b2fcace731e4e
Pabloin <pablo.inchausti.bf@gmail.com>
2019-08-09 20:18:12 -0300

    Tests

--- class7/app-colegio/src/test/java/bbva/theninjaproject/carrerajava/clase07/curricula/MateriaTest.java
+++ class7/app-colegio/src/test/java/bbva/theninjaproject/carrerajava/clase07/curricula/MateriaTest.java
@@ -3,19 +3,28 @@ package bbva.theninjaproject.carrerajava.clase07.curricula;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MateriaTest {
 	
+	Materia quimica;
+	Materia matematica;
+	Materia literatura;
+	Materia fisica;
+	
+	@Before
+	public void test() {
+		
+		quimica    = new Materia("Quimina",    Materia.EXAMEN_FINAL,   4);
+		matematica = new Materia("Matemática", Aprobable.EXAMEN_FINAL, 7);
+		literatura = new Materia("Literatura", Aprobable.ENTREGA_TF);
+		fisica     = new Materia("Fisica",     Aprobable.PROMOCIONA);
+	}
+	
 	@Test
 	public void testMaterias() {
 
-		Materia quimica    = new Materia("Quimina",    Materia.EXAMEN_FINAL,   4);
-		Materia matematica = new Materia("Matemática", Aprobable.EXAMEN_FINAL, 7);
-		Materia literatura = new Materia("Literatura", Aprobable.ENTREGA_TF);
-		Materia fisica     = new Materia("Fisica",     Aprobable.PROMOCIONA);
-		
-		
 		assertFalse(quimica.aprovado(2));
 		assertTrue(quimica.aprovado(4));
 		assertTrue(quimica.aprovado(6));
